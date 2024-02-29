b665eaba2f2e805a764e9c59810b460253cee33a
zohaib240 <zohaibjs123@gmail.com>
2024-02-29 10:18:35 +0500

    index file created dand doc.js

---
+++ doc.js
@@ -0,0 +1,19 @@
+var num =+prompt('(Question no 1) \n enter number \n');
+if(num % 3 ===0){
+    console.log('the number is divisible by 3')
+}
+else if(num){
+    console.log('the number is not divisible by 3')
+    
+}
+
+// Question no 2
+
+var num =+prompt('(Question no 2) \n enter number \n');
+if(num % 2 ===0){
+    console.log('the number is even')
+}
+else if(num){
+    console.log('the number is odd')
+    
+}
