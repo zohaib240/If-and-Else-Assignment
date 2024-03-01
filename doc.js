15253893edef5033b01ee7658de821913e662df1
zohaib240 <zohaibjs123@gmail.com>
2024-03-01 07:54:58 +0500

    index file changed

--- doc.js
+++ doc.js
@@ -17,3 +17,31 @@ else if(num){
     console.log('the number is odd')
     
 }
+
+
+// Question no 3
+
+
+var num =+prompt('(Question no 3) \n enter number \n');
+if(num > 18){
+    console.log('Old Enough')
+}
+else if(num < 18){
+    console.log('Too young')
+    
+}
+
+
+// Question 4
+
+
+
+var name =prompt('(Question no 4) \n Enter Your Name \n');
+if(name ===zohaib){
+    console.log('Asslamoalikum')
+}
+else{
+    console.log('else')
+}
+
+
